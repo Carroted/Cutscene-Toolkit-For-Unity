bac2dbde82a489882f91af2f4e63a0f9923eaad2
Carroted <alexsour@protonmail.com>
2021-11-12 14:22:47 -0400

    Added a lot

---
+++ Assets/TaggedObjectSensor.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEditor;
+using System.Linq;
+
+namespace CutsceneToolkit
+{
+
+    public class TaggedObjectSensor : MonoBehaviour
+    {
+        public UnityEvent onObjectEnter;
+        public UnityEvent onObjectStay;
+        public UnityEvent onObjectExit;
+
+        public string[] tags = new string[] { };
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (tags.Length == 0)
+                return;
+
+            if (tags.Contains(other.tag))
+                onObjectEnter.Invoke();
+        }
+        void OnTriggerStay2D(Collider2D other)
+        {
+            if (tags.Length == 0)
+                return;
+
+            if (tags.Contains(other.tag))
+                onObjectStay.Invoke();
+        }
+        void OnTriggerExit2D(Collider2D other)
+        {
+            if (tags.Length == 0)
+                return;
+
+            if (tags.Contains(other.tag))
+                onObjectExit.Invoke();
+        }
+    }
+
+}
